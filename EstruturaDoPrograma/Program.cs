3557b9e4a80ef7dec610781d8253a1bc0962d975
braziltaiany <braziltaiany@outlook.com>
2021-09-09 16:23:21 -0300

    chore: Iniciando Aplicacao - Estrutura do projeto

---
+++ EstruturaDoPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
