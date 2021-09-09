0faa3649f1036b96b39888c4032cc639f2b6d8ac
braziltaiany <braziltaiany@outlook.com>
2021-09-09 14:49:50 -0300

    chore: Aplicacao Console

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-Vindo ao Curso de  .NET{i}");
+            }
+        }
+    }
+}
