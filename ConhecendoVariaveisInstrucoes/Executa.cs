dab223d2994bffae71f10c0e3f2b80204e4193a4
braziltaiany <braziltaiany@outlook.com>
2021-09-10 16:56:15 -0300

    feat: adicionando metodo - InstrucaoIf

---
+++ ConhecendoVariaveisInstrucoes/Executa.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+
+ namespace ConhecendoVariaveisInstrucoes
+ {
+    class Executar
+    {
+         static void Main(string[] args)
+         {
+            Program instrucoes = new Program();
+            int soma = instrucoes.Declaracoes();
+            Console.WriteLine(soma);
+
+            int[] arr = { 25, 10, 20, 15, 40, 50 };
+            instrucoes.InstrucaoIf(arr);
+
+            string[] args = {"1","2","3"}
+            instrucoes.InstrucaoSwitch(args)
+         }
+     }
+ } 
+
--- ConhecendoVariaveisInstrucoes/Instrucoes.cs
+++ ConhecendoVariaveisInstrucoes/Instrucoes.cs
@@ -13,12 +13,25 @@ namespace ConhecendoVariaveisInstrucoes
             a = 3;
 
             return a + b + c + d;
-           
         }
 
+        public void InstrucaoIf(int[] arr)
+        {               
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("Nenhum argumento");
+            }
+            else if (arr.Length == 1)
+            {
+                Console.WriteLine("Um argumento");
+            }
+            else
+            {
+                Console.WriteLine($"{arr.Length} argumentos");
+            }
+        }
 
-
-
-    
+        
+        }    
     }
 }
