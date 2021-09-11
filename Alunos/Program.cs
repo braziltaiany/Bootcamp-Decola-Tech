9beb5a75efcbb9d5bddfba426ec97d96fbd03878
braziltaiany <braziltaiany@outlook.com>
2021-09-11 12:00:22 -0300

    Chore: Inserindo novo projeto Alunos

--- Alunos/Program.cs
+++ Alunos/Program.cs
@@ -6,7 +6,34 @@ namespace Alunos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                 switch (opcaoUsuario)
+                 {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        break;
+                    case "3":
+                        //TODO: calcular média geral
+                        break;
+                         
+                    default: throw new ArgumentOutOfRangeException();
+
+                 }
+                    Console.WriteLine("Informe a opção desejada:");
+                    Console.WriteLine("1- Inserir Novo Aluno");
+                    Console.WriteLine("2- Listar Alunos");
+                    Console.WriteLine("3- Calcular média geral");
+                    Console.WriteLine("X- Sair");
+                    Console.WriteLine();
+                    
+                    opcaoUsuario = Console.ReadLine();
+            }
         }
     }
 }
