acd6fe32681b8093cabe332f1853bf3066182c5a
braziltaiany <braziltaiany@outlook.com>
2021-09-13 19:44:17 -0300

    chore: refatoracao para reaproveitamento de codigo criando uma nova classe Endereco()

--- AgenciaBancaria/AgenciaBancaria.Dominio/Cliente.cs
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Cliente.cs
@@ -8,23 +8,18 @@ namespace AgenciaBancaria.Dominio
 {
     public class Cliente
     {
-        public Cliente(string nome, string cpf, string rg, string logradouro, string cep, string cidade, string estado)
+        public Cliente(string nome, string cpf, string rg, Endereco endereco)
         {
             Nome = nome.ValidaStringVazia();
             CPF = cpf.ValidaStringVazia();
             RG = rg.ValidaStringVazia();
-            Logradouro = logradouro.ValidaStringVazia();
-            CEP = CEP.ValidaStringVazia();
-            Cidade = cidade.ValidaStringVazia();
-            Estado = estado.ValidaStringVazia();
-
+            Endereco = endereco;
+            
         }
 
         public string Nome { get; private set; }
         public string CPF { get; private set; }
         public string RG { get; private set; }
-        public string Logradouro { get; private set; }
-        public string CEP { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
-}   }
+        public Endereco Endereco { get; private set; }
+
+    }   }
--- AgenciaBancaria/AgenciaBancaria.Dominio/Endereco.cs
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Endereco.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace AgenciaBancaria.Dominio
 {
-    class Endereco
+    public class Endereco
     {
+        public Endereco(string logradouro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro.ValidaStringVazia();
+            CEP = cep.ValidaStringVazia();
+            Cidade = cidade.ValidaStringVazia();
+            Estado = estado.ValidaStringVazia();
+        }
+
+        public string Logradouro { get; private set; }
+        public string CEP { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
     }
 }
