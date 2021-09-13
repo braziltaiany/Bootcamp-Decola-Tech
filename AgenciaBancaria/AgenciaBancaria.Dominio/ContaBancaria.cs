310689e6803d37a01562761286853ee53cbdeca8
braziltaiany <braziltaiany@outlook.com>
2021-09-13 20:18:13 -0300

    Utilizando o Random para garantir que minha conta sempre quando criar sempre terá numero da conta e digito verificador

--- AgenciaBancaria/AgenciaBancaria.Dominio/ContaBancaria.cs
+++ AgenciaBancaria/AgenciaBancaria.Dominio/ContaBancaria.cs
@@ -8,13 +8,18 @@ namespace AgenciaBancaria.Dominio
 {
     public class ContaBancaria
     {
-        public ContaBancaria()
+        //construtor
+       public ContaBancaria(Cliente cliente)
         {
+            // estou garantindo que minha conta sempre quando criar sempre terá numero da conta e digito verificador
+            Random random = new Random();
+            NumeroConta = random.Next(50000, 100000);
+            DigitoVerificador = random.Next(0, 9);
             Situacao = SituacaoConta.Criada;
         }
-        public int NumeroConta { get; init; }
-        public int DigitoVerificador { get; init; }
-        public decimal Saldo { get; protected set; }
+        public int NumeroConta { get; init; }// init significa so posso setar os valores de NumeroConta na inicialização da minha classe no construtor
+        public int DigitoVerificador { get; init; }// NumeroConta esse campo não podem ser modificados
+        public decimal Saldo { get; protected set; }// minhas classes filhas, as classes que herdarem pode setar a propriedade saldo
         public DateTime? DataAbertura { get; private set; }
         public DateTime? DataEncerramento { get; private set; }
         public SituacaoConta Situacao { get; private set; }
