8a5bf68ea97d86e425474b4967df8df498baa301
braziltaiany <braziltaiany@outlook.com>
2021-09-13 13:47:16 -0300

    Chore: iniciando a aplicacao - adicionando classes e criando objeto

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AgenciaBancaria.Dominio;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente(
+                "Taiany", 
+                "123456", 
+                "1233733733", 
+                "teste", 
+                "12345678", 
+                "rio de janeiro", 
+                "RJ");
+
+            Console.WriteLine(cliente.Nome);
+        }
+
+    }
+}
---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente
+    {
+        //Criando um construtor
+        public Cliente(string nome, string cpf, string rg, string logradouro, string cep, string cidade, string estado) 
+        {
+            //verificando se a string está vazia com o método string.IsNullOrWhiteSpace() 
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new Exception("Propriedade deve estar preenchida.");
+            }
+
+
+            Nome = nome;
+            
+            // ou podemos utilizar o operador ? que faz a mesma coisa que o if acima
+            CPF = string.IsNullOrWhiteSpace(cpf) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : cpf;
+
+            RG = string.IsNullOrWhiteSpace(rg) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : rg;
+
+            Logradouro = string.IsNullOrWhiteSpace(logradouro) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : logradouro;
+
+            CEP = string.IsNullOrWhiteSpace(cep) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : cep;
+
+            Cidade = string.IsNullOrWhiteSpace(cidade) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : cidade;
+
+            Estado = string.IsNullOrWhiteSpace(estado) ?
+                throw new Exception("Propriedade deve estar preenchida.")
+                : estado;
+        }
+        
+        //acessor public e propriedades Nome, somente dentro da classe nome podemos setar a propriedade nome
+        //PROP+ TAB duas vezes adiciona uma propriedade automaticamente
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public string Logradouro { get; private set; }
+        public string CEP { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+
+    }
+}
