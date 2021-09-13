1d1e90644cfa32ace5d8deab987c4ec96b724c2e
braziltaiany <braziltaiany@outlook.com>
2021-09-13 19:55:33 -0300

    chore: criando nova classe ContaBancaria

---
+++ AgenciaBancaria/AgenciaBancaria.Dominio/ContaBancaria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta { get; init; }
+        public int DigitoVerificador { get; init; }
+        public decimal Saldo { get; protected set; }
+        public DateTime? DataAbertura { get; private set; }
+        public DateTime? DataEncerramento { get; private set; }
+        public SituacaoConta Situacao { get; private set; }
+        public string Senha { get; private set; }
+        public Cliente Cliente { get; init; }
+        public List<Lancamento> Lancamentos { get; private set; }
+    }
+}
