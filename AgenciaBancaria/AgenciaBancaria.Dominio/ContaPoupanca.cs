f409035cdbb552b6f4ea187828e2b7381cf83f13
braziltaiany <braziltaiany@outlook.com>
2021-09-13 22:02:49 -0300

    Chore: adicionando contrutor e adicionando propriedades na classe ContaPoupanca()

--- AgenciaBancaria/AgenciaBancaria.Dominio/ContaPoupanca.cs
+++ AgenciaBancaria/AgenciaBancaria.Dominio/ContaPoupanca.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace AgenciaBancaria.Dominio
 {
-    class ContaPoupanca
+    //Conta poupança herda de contabancaria, tudo que conta bancaria tem a conta poupança também tem.
+
+    public class ContaPoupanca : ContaBancaria
     {
+        //criaando um construtor 
+        //herda da base mãe Contabancaria - F12 em cima da  base mostra isso
+        public ContaPoupanca(Cliente cliente): base(cliente)
+        {
+            //Para clientes - não precisa fazer mais nada aqui porque já herda
+
+            //0,30%
+            PercentualRendimento = 0.003M;
+
+        }
+
+        public decimal PercentualRendimento { get; private set; }
     }
 }
