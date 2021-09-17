4fee97898cef0bb97b30e0b744249a175cd5b525
braziltaiany <braziltaiany@outlook.com>
2021-09-17 19:40:32 -0300

    Adicionando AddScoped para criar instancias automaticas

--- ApiCatalogoJogos/ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiCatalogoJogos.Services;
+using ExemploApiCatalogoJogos.Repositories;
+using ExemploApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +29,8 @@ namespace ApiCatalogoJogos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
