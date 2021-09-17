edf12f7b7fda8999e85bad8cdac55016403ea0f2
braziltaiany <braziltaiany@outlook.com>
2021-09-17 18:46:10 -0300

    Criando entidades

--- ApiCatalogoJogos/ApiCatalogoJogos/Repositories/IJogoRepository.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using ExemploApiCatalogoJogos.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace ApiCatalogoJogos.Repositories
+namespace ExemploApiCatalogoJogos.Repositories
 {
-    public interface IJogoRepository
+    public interface IJogoRepository : IDisposable
     {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
     }
 }
+
