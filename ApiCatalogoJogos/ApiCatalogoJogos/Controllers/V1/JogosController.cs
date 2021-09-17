22b5e47223ea677b7313500ec1dcf5e42eecb211
braziltaiany <braziltaiany@outlook.com>
2021-09-17 14:28:32 -0300

    Iniciando o projeto

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    //deixar claro a versão do projeto para quem vai consumir a api - V1
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        //crud
+        // task utilizado para garantir uma melhor performance requisições web
+        //async ele vai esperar um ActionResult de uma listagem de objetos
+        //actionResult é um tipo de retorno, dos status HTTp
+        //método
+        public async Task<ActionResult<List<Object>>> Obter()
+        {
+            return Ok();
+        }
+    }
+}
