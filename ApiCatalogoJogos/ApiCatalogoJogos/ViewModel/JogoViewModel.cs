c84ef881a48df6df81c95eea66e17f90da75807a
braziltaiany <braziltaiany@outlook.com>
2021-09-17 16:23:38 -0300

    Adicionando  classe JogoViewModel

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Produtora { get; set; }
+
+        public double Preco { get; set; }
+    }
+}
