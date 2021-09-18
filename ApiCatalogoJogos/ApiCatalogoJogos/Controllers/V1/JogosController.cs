7a64c620b24695c419f44e4c732c0dffc3c5da5b
braziltaiany <braziltaiany@outlook.com>
2021-09-17 21:25:21 -0300

    Adicionando novas funcionalidades

--- ApiCatalogoJogos/ApiCatalogoJogos/Controllers/V1/JogosController.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -1,37 +1,38 @@
-﻿using System;
+﻿using ApiCatalogoJogos.inputModel;
+using ApiCatalogoJogos.Services;
+using ApiCatalogoJogos.ViewModel;
+using ExemploApiCatalogoJogos.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using ApiCatalogoJogos.inputModel;
-using ApiCatalogoJogos.Services;
-using ApiCatalogoJogos.ViewModel;
-using ExemploApiCatalogoJogos.Exceptions;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
-namespace ApiCatalogoJogos.Controllers.V1
+namespace ExemploApiCatalogoJogos.Controllers.V1
 {
-    //deixar claro a versão do projeto para quem vai consumir a api - V1
-    [Route("api/V1/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class JogosController : ControllerBase
     {
-        //interface
         private readonly IJogoService _jogoService;
 
-        //ganhamos uma instancia
         public JogosController(IJogoService jogoService)
         {
             _jogoService = jogoService;
         }
 
-
+        /// <summary>
+        /// Buscar todos os jogos de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os jogos sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de reistros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de jogos</response>
+        /// <response code="204">Caso não haja jogos</response>   
         [HttpGet]
-        // task utilizado para garantir uma melhor performance requisições web
-        //async ele vai esperar um ActionResult de uma listagem de objetos
-        //actionResult é um tipo de retorno, dos status HTTp
-        //método
         public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
             var jogos = await _jogoService.Obter(pagina, quantidade);
@@ -42,10 +43,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(jogos);
         }
 
+        /// <summary>
+        /// Buscar um jogo pelo seu Id
+        /// </summary>
+        /// <param name="idJogo">Id do jogo buscado</param>
+        /// <response code="200">Retorna o jogo filtrado</response>
+        /// <response code="204">Caso não haja jogo com este id</response>   
         [HttpGet("{idJogo:guid}")]
-
-        //Contratos injeção de dependencia
-        //Tem que depender de contratos, recebo um contrato que é uma interface de um serviço que foi implantado. No caso a classe IJogoService
         public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
         {
             var jogo = await _jogoService.Obter(idJogo);
@@ -56,6 +60,12 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(jogo);
         }
 
+        /// <summary>
+        /// Inserir um jogo no catálogo
+        /// </summary>
+        /// <param name="jogoInputModel">Dados do jogo a ser inserido</param>
+        /// <response code="200">Cao o jogo seja inserido com sucesso</response>
+        /// <response code="422">Caso já exista um jogo com mesmo nome para a mesma produtora</response>   
         [HttpPost]
         public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel)
         {
@@ -71,7 +81,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
-        //put eu atualizo o conteudo inteiro
+        /// <summary>
+        /// Atualizar um jogo no catálogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser atualizado</param>
+        /// <param name="jogoInputModel">Novos dados para atualizar o jogo indicado</param>
+        /// <response code="200">Cao o jogo seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
         [HttpPut("{idJogo:guid}")]
         public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
         {
@@ -87,7 +103,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
-        //no patch eu atualizo uma coisa especifica, exemplo: só o preço do jogo
+        /// <summary>
+        /// Atualizar o preço de um jogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser atualizado</param>
+        /// <param name="preco">Novo preço do jogo</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
         [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
         public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromRoute] double preco)
         {
@@ -103,6 +125,12 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Excluir um jogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser excluído</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
         [HttpDelete("{idJogo:guid}")]
         public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
         {
@@ -120,3 +148,4 @@ namespace ApiCatalogoJogos.Controllers.V1
 
     }
 }
+
--- ApiCatalogoJogos/ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/ApiCatalogoJogos/Startup.cs
@@ -1,21 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ApiCatalogoJogos.Services;
+using ExemploApiCatalogoJogos.Controllers.V1;
 using ExemploApiCatalogoJogos.Repositories;
 using ExemploApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
-namespace ApiCatalogoJogos
+namespace ExemploApiCatalogoJogos
 {
     public class Startup
     {
@@ -26,38 +23,41 @@ namespace ApiCatalogoJogos
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IJogoService, JogoService>();
             services.AddScoped<IJogoRepository, JogoRepository>();
+
+            #region CicloDeVida
+
+            services.AddSingleton<IExemploSingleton, ExemploCicloDeVida>();
+            services.AddScoped<IExemploScoped, ExemploCicloDeVida>();
+            services.AddTransient<IExemploTransient, ExemploCicloDeVida>();
+
+            #endregion
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogoJogos", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExemploApiCatalogoJogos", Version = "v1" });
+
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCatalogoJogos v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExemploApiCatalogoJogos v1"));
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+           
         }
     }
 }
