bef5d28784ce7b649db7c1b1bf0ecaf0e5dff7a6
braziltaiany <braziltaiany@outlook.com>
2021-09-18 16:29:09 -0300

    Criando metodo get para listar series

---
+++ CatalogoSeries/WebApiSerie/Controllers/SerieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSerie.Controllers
+{ 
+    [Route("[controller]")]
+    public class SerieController : Controller
+    {
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            IList<SerieModel> series = new List<SerieModel>();
+
+            series.Add(new SerieModel() { Titulo = "Titulo Série 1" });
+            series.Add(new SerieModel() { Titulo = "Titulo Série 2" });
+            series.Add(new SerieModel() { Titulo = "Titulo Série 3" });
+            series.Add(new SerieModel() { Titulo = "Titulo Série 4" });
+
+            return Ok(series);
+        }
+    }
+}
--- CatalogoSeries/WebApiSerie/SerieModel.cs
+++ CatalogoSeries/WebApiSerie/SerieModel.cs
@@ -7,5 +7,12 @@ namespace WebApiSerie
 {
     public class SerieModel
     {
+        public int Id { get; set; }        
+        private CatalogoSeries.Series.Genero Genero { get; set; }
+       // public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public bool Excluido { get; set; }
     }
 }
